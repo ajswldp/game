cf313c910895d27dec5a543cd759197fac443df7
ajswl <kkhhjj1209@gmail.com>
2025-05-23 18:56:13 +0900

    jwt

--- src/device/device.gateway.ts
+++ src/device/device.gateway.ts
@@ -1,12 +1,25 @@
-import { WebSocketGateway, SubscribeMessage, MessageBody } from '@nestjs/websockets';
+import { WebSocketGateway, SubscribeMessage, MessageBody, WebSocketServer } from '@nestjs/websockets';
 import { DeviceService } from './device.service';
 import { CreateDeviceDto } from './dto/create-device.dto';
 import { UpdateDeviceDto } from './dto/update-device.dto';
+import { Server, Socket } from 'socket.io';
 
 @WebSocketGateway()
 export class DeviceGateway {
   constructor(private readonly deviceService: DeviceService) {}
 
+  @WebSocketServer()
+  server: Server;
+
+  handleConnection(client: Socket) {
+    console.log('Client connected:', client.id);
+  }
+
+  @SubscribeMessage('message')
+  handleMessage(client: Socket, payload: any): void {
+    console.log('Message received:', payload);
+    this.server.emit('message', payload); // 모두에게 메시지 전송
+  }
   @SubscribeMessage('createDevice')
   create(@MessageBody() createDeviceDto: CreateDeviceDto) {
     return this.deviceService.create(createDeviceDto);
