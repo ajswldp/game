1681bf1e54164cdd2d12553e2fb49f4ccb34e702
ajswldp <kkhhjj1209@gmail.com>
2025-06-04 15:05:18 +0900

    cros

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,13 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     httpsOptions,
   });
+  app.enableCors({
+    origin: true,      // 모든 origin 허용
+    methods: '*',      // 모든 HTTP 메서드 허용
+    allowedHeaders: '*', // 모든 헤더 허용
+    credentials: true,   // 필요한 경우 함께 사용, 아니면 생략
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
