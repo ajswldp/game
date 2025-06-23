e1d2099382f85a6d7b22451de631af01c0670123
ajswldp <kkhhjj1209@gmail.com>
2025-06-23 15:48:48 +0900

    name 매핑

--- src/host/host.controller.ts
+++ src/host/host.controller.ts
@@ -1,10 +1,4 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  UseGuards,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
 import { HostService } from './host.service';
 import { SignupHostDto } from './dto/signup-host.dto';
 import { JwtAuthGuard } from '../auth/jwt/jwt.guard';
@@ -14,6 +8,7 @@ import { User } from '../auth/user/user';
 import { LoginDto } from './dto/login.dto';
 import { RolesGuard } from '../auth/role.guard';
 import { DistanceInfo } from '../device/dto/info.dto';
+import { NameDto } from './dto/name.dto';
 
 @Controller('host')
 export class HostController {
@@ -28,6 +23,13 @@ export class HostController {
   ) {
     await this.hostService.distance(user, distanceInfo);
   }
+  @UseGuards(JwtAuthGuard, RolesGuard)
+  @Roles('host')
+  @Post('/name')
+  async name(@CurrentUser() user: User, @Body() nameDto: NameDto) {
+    await this.hostService.name(user, nameDto);
+  }
+
   @Post('/signup')
   async signup(@Body() signupHostDto: SignupHostDto) {
     await this.hostService.signup(signupHostDto);
